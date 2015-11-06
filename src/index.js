c6869eab47ba40c687ea827c654a671ada884520
TakesxiSximada <takesxi.sximada@gmail.com>
2015-11-07 00:29:27 +0900

    pages ディレクトリにコンポーネントを移動

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 // -*- coding: utf-8 -*-
 var m = require('mithril');
-var page_components = require('./components.js');
-var views = require('msx-loader!./views.msx');
-var controllers = require('./controllers.js');
+var pages_components = require('./pages/components.js');
 
 
 m.route(document.querySelector('#pages'), '/', {
-    '/': page_components.list_page,
-    '/create': page_components.create_page,
-    '/show/:page_id:': page_components.show_page,
-    '/update/:page_id:': page_components.update_page,
-    '/delete/:page_id:': page_components.delete_page,
+    '/': pages_components.list_page,
+    '/create': pages_components.create_page,
+    '/show/:page_id:': pages_components.show_page,
+    '/update/:page_id:': pages_components.update_page,
+    '/delete/:page_id:': pages_components.delete_page,
 });
